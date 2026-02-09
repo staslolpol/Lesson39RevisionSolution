6003c6e57f65e75e6227b36e1960075a751317a4
staslolpol <staspriho9@gmail.com>
2026-02-09 18:47:47 +0300

    dvnvnn

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -1,5 +1,22 @@
 ﻿#include "logic.h"
 
-long long get_fibo_number(int index) {
-	return 0LL;
+long long get_fibo_number(int n) {
+	int result;
+
+	if (n < 0) {
+		return -1;
+	}
+
+	int number1 = 0;
+	int number2 = 1;
+	int number3;
+
+	for (int i = 1; i < n; i++) {
+
+
+		number3 = number1 + number2;
+		number1 = number2;
+		number2 = number3;
+	}
+	return number2;
 }
